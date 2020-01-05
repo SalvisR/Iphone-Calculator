c09fd991321edd25f4bcd4f9a56bfa3bc96e5088
Salvis Reimanis <salvis64@inbox.lv>
2020-01-05 13:13:48 +0200

    Set font size and check string length

--- js/app.js
+++ js/app.js
@@ -12,30 +12,30 @@ let screenNumber = '';
 // Converts string to number
 const toNumber = num => {
   return num * 1;
-}
+};
 
 const math = {
-  '+': (sum) => {
-    return function (x) {
-      return result = sum + x;
-    }
+  '+': sum => {
+    return function(x) {
+      return (result = sum + x);
+    };
   },
-  '-': (sum) => {
-    return function (x) {
-      return result = sum - x;
-    }
+  '-': sum => {
+    return function(x) {
+      return (result = sum - x);
+    };
   },
-  '*': (sum) => {
-    return function (x) {
-      return result = sum * x;
-    }
+  '*': sum => {
+    return function(x) {
+      return (result = sum * x);
+    };
   },
-  '/': (sum) => {
-    return function (x) {
-      return result = sum / x;
-    }
+  '/': sum => {
+    return function(x) {
+      return (result = sum / x);
+    };
   }
-}
+};
 
 // Clear all
 ac.addEventListener('click', () => {
@@ -51,11 +51,11 @@ sumBtn.addEventListener('click', () => {
   action = '';
   fn = null;
   screen.textContent = result;
+  setFontSize();
 });
 
 numbers.forEach(number => {
   number.addEventListener('click', e => {
-
     // Checks if number already contains comma
     const comma = screenNumber.includes('.');
     if (e.target.textContent === '.' && comma) {
@@ -63,7 +63,6 @@ numbers.forEach(number => {
     }
 
     if (screen.textContent === '0') {
-
       if (e.target.textContent === '.') {
         screenNumber = '0.';
         screen.textContent = screenNumber;
@@ -73,30 +72,33 @@ numbers.forEach(number => {
       }
     } else {
       if (action !== '') {
+        if (screenNumber.length > 8) {
+          return;
+        }
         screenNumber += e.target.textContent;
         screen.textContent = screenNumber;
         result = fn(toNumber(screenNumber));
-
       } else {
+        if (screenNumber.length > 8) {
+          return;
+        }
         screenNumber += e.target.textContent;
         screen.textContent = screenNumber;
-
       }
     }
+
+    setFontSize();
   });
 });
 
 mathBtns.forEach(btn => {
   btn.addEventListener('click', () => {
-
     if (btn.dataset.math === '%') {
       screenNumber = toNumber(screenNumber) / 100;
       screen.textContent = screenNumber;
-
     } else if (btn.dataset.math === '+/-') {
       screenNumber = -1 * toNumber(screenNumber);
       screen.textContent = screenNumber;
-
     } else {
       screenNumber = '';
       if (toNumber(result) !== 0) {
@@ -106,5 +108,16 @@ mathBtns.forEach(btn => {
       result = toNumber(screen.textContent);
       fn = math[action](result);
     }
-  })
+    setFontSize();
+  });
 });
+
+const setFontSize = () => {
+  if (screen.textContent.toString().length > 8) {
+    screen.style.fontSize = '2.5rem';
+  } else if (screen.textContent.toString().length > 6) {
+    screen.style.fontSize = '3rem';
+  } else {
+    screen.style.fontSize = '4rem';
+  }
+};
